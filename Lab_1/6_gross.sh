fe5a72250187892984968ea4ea66f217ab3edd83
Mohammed Abdul Hamid <68508434+hamid077@users.noreply.github.com>
2021-12-02 10:40:42 +0530

    Update 6_gross.sh

--- Lab_1/6_gross.sh
+++ Lab_1/6_gross.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-echo "enter basic salary"
-read basic
-da=`echo $basic \* 0.1|bc`
-hra=`echo $basic \* 0.2|bc`
-gross=`echo (scale=2);$da \+ $hra \+ $basic|bc`
-echo $gross
+echo "Enter the basic salary"
+read bsal
+da=`expr "$bsal "*" 0.1" | bc`
+hra=`echo "$bsal "*" 0.2" | bc`
+gsal=`echo "$bsal + $da + $hra" |bc`
 
-
+echo "Gross salary is: $gsal"
