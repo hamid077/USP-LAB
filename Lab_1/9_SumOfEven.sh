e76a054dec6c9a128a5ac75c5001ae1a37383eb1
Mohammed Abdul Hamid <68508434+hamid077@users.noreply.github.com>
2021-12-07 19:19:20 +0530

    Update 9_SumOfEven.sh

--- Lab_1/9_SumOfEven.sh
+++ Lab_1/9_SumOfEven.sh
@@ -1,12 +1,15 @@
-#!/bin/sh
-echo "Enter the number of elements"
+#!/bin/bash
+echo "enter a number"
 read n
 sum=0
-echo "Enter the numbers"
 while [ $n -gt 0 ]
-do
-	read a
-	sum=$((sum+a))
-	n=$((n-1))
+do 
+value=`expr $n % 2`
+if [ $value -eq 0 ]
+then
+sum=`expr $sum + $n`
+fi
+n=`expr $n - 1`
 done
-echo "Sum is $sum"
+echo "the total sum is :"
+echo "$sum"
