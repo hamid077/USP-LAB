8cf47ccfd74c7fb757dce1b4aca8a62310871b44
Mohammed Abdul Hamid <68508434+hamid077@users.noreply.github.com>
2021-12-07 18:38:26 +0530

    Update 8_arithmetic.sh

--- Lab_1/8_arithmetic.sh
+++ Lab_1/8_arithmetic.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-echo "enter 2 nos"
-read n1
-read n2
-minus=`echo $n1\-$n2|bc`
-add=`echo $n1 \+ $n2|bc`
-mul=`echo $n1 \* $n2|bc`
-div=`echo $n1 \/ $n2|bc`
-mod=`echo $n1 \% $n2|bc`
-echo "subtraction:$minus"
-echo "addition:$add"
-echo "mutiplicatio:$mul"
-echo "division:$div"
-echo "remainder:$mod"
-
-
-
+echo "1.Add\n2.Sub\n3.Mul\n4.Div"
+read i
+echo "number the two numbers"
+read a
+read b
+case $i in
+1)ans=$((a+b));;
+2)ans=$((a-b));;
+3)ans=$((a*b));;
+4)ans=`echo "scale=2;$a/$b"|bc`;;
+*)echo "enter valid input"
+esac
+echo $ans
