97d6498d62e15fade118647dd41dde1364fde576
Mohammed Abdul Hamid <68508434+hamid077@users.noreply.github.com>
2021-12-07 18:37:53 +0530

    Update 7_cel.sh

--- Lab_1/7_cel.sh
+++ Lab_1/7_cel.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-echo "enter the temparature in fahrenheit"
-read temp
-y=`echo "scale=2;5/9"|bc`
-x=`echo $temp\-32|bc`
-ans=`echo $x\*$y|bc`
-echo "temp in celcius:$ans"
+echo "Enter degree fahrenheit temperature: " 
+read f
+c=`echo "scale=4; ($f - 32) / 1.8" | bc`
+echo "$f degree fahrenheit is equal to $c degree celsius
